b21ebe904dc05a99e90ec112beb75ba587ac3b77
Pascal Nasahl <nasahl@google.com>
2022-01-18 18:13:19 +0000

    Consider the output pin when creating the formula.
    When building the boolean formula, we also need to consider the output
    pin of the current node. Only connect two nodes if the pin matches.
    
    Signed-off-by: Pascal Nasahl <nasahl@google.com>

--- formula_class.py
+++ formula_class.py
@@ -54,42 +54,46 @@ class FormulaBuilder:
                         node, data=True):
                     inputs = {}
                     port_name = out_edge_data["edge"].out_port
+                    pin_name = out_edge_data["edge"].out_pin
                     node_type_out = node_type + "_" + port_name
                     # Assemble the name of the current node
                     # (=node_name+out_port_name) and translate the name to an
                     # integer value needed by the SAT solver.
-                    node_name = node + "_" + port_name
+                    node_name = node + "_" + port_name + str(pin_name)
                     if node_name not in node_int:
                         node_int[node_name] = node_int_cntr
                         node_int_cntr += 1
                     inputs["node_name"] = InputPin(node, node_int[node_name])
-
                     # Loop over all input edges of the current node to find the
                     # nodes connected to the current node.
                     for in_node_out, in_node_in, in_edge_data in self.graph.in_edges(
                             node, data=True):
-                        in_node_type = self.graph.nodes[in_node_out][
-                            "node"].type
-                        # Get the name of the node (=in_node_out+out_port_name)
-                        # connected to the input pin of the current node.
-                        input_name = in_node_out + "_" + in_edge_data[
-                            "edge"].out_port
-                        # Get the name of the input port.
-                        in_port = in_edge_data["edge"].in_port
-                        # Add the name of the node + the in port to the input dict.
-                        # Connect zero/ones with the corresponding element.
-                        if in_node_type == "null_node":
-                            inputs[in_port] = InputPin(input_name,
-                                                       self.cell_lib.zero)
-                        elif in_node_type == "one_node":
-                            inputs[in_port] = InputPin(input_name,
-                                                       self.cell_lib.one)
-                        else:
-                            if input_name not in node_int:
-                                node_int[input_name] = node_int_cntr
-                                node_int_cntr += 1
-                            inputs[in_port] = InputPin(input_name,
-                                                       node_int[input_name])
+                        if int(pin_name) == int(in_edge_data["edge"].in_pin):
+                            not_connected = True
+                            in_node_type = self.graph.nodes[in_node_out][
+                                "node"].type
+                            # Get the name of the node.
+                            # (=in_node_out+out_port_name+out_pin)
+                            # connected to the input pin of the current node.
+                            input_name = in_node_out + "_" + in_edge_data[
+                                "edge"].out_port + str(
+                                    in_edge_data["edge"].out_pin)
+                            # Get the name of the input port.
+                            in_port = in_edge_data["edge"].in_port
+                            # Add the name of the node + the in port to the input dict.
+                            # Connect zero/ones with the corresponding element.
+                            if in_node_type == "null_node":
+                                inputs[in_port] = InputPin(
+                                    input_name, self.cell_lib.zero)
+                            elif in_node_type == "one_node":
+                                inputs[in_port] = InputPin(
+                                    input_name, self.cell_lib.one)
+                            else:
+                                if input_name not in node_int:
+                                    node_int[input_name] = node_int_cntr
+                                    node_int_cntr += 1
+                                inputs[in_port] = InputPin(
+                                    input_name, node_int[input_name])
 
                     # If there is an input port with input size greater than 1
                     # than we have a predefined input value of one/zero for this
@@ -125,5 +129,4 @@ class FormulaBuilder:
                                     logger.error(
                                         f"Err: Gate type {node_type_out} not found."
                                     )
-
         return self.solver
